ccef24bb541c12a0384c2794aad08492a305f86d
Nathan Condell <nate@Justin-Cressys-MacBook-Pro.local>
2014-04-06 22:30:06 -0700

    Initial Commit

---
+++ ABMA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ABMA
+//
+//  Created by Nathan Condell on 4/6/14.
+//  Copyright (c) 2014 Nathan Condell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ABMA/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ABMA
+//
+//  Created by Nathan Condell on 4/6/14.
+//  Copyright (c) 2014 Nathan Condell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
