d651ed4f8201e9ca6c91e17c8318b456a05031a7
Nathan Condell <natecondell@gmail.com>
2016-04-01 06:55:47 -0700

    Add links. Reorder sponsors

--- ABMA/SponsorCollectionViewController.m
+++ ABMA/SponsorCollectionViewController.m
@@ -16,6 +16,7 @@
 @implementation SponsorCollectionViewController
 {
     NSArray *sponsorImages;
+    NSArray *links;
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -34,7 +35,36 @@
     //Sponsor image array
 //    sponsorImages = [[NSArray alloc] initWithObjects:@"AAZK-Dallas.png", @"AAZK-Galv.png", @"AAZKChey.png", @"ABI.png", @"AP.png", @"Blue.png", @"ChildrensAquarium.png", @"Cliff.png", @"DallasZoo.png", @"DWA.png", @"FRWC.png", @"FWZoo.png", @"MAF.png", @"NatBal.png", @"NEI.png", @"SeaWorld.png", nil];
 //    sponsorImages = [[NSArray alloc] initWithObjects:@"DBP.png", @"CPHZoo.png", @"GIVSKUD_ZOO.png", @"Odense Zoo.png", @"SDU.png", @"training_store.png", @"mazuri.png", @"profis.png", @"sea_world.png", @"zooply.png",  nil];
-    sponsorImages = [[NSArray alloc] initWithObjects:@"ABI Logo.jpg", @"BGT Logo.png", @"brevard_zoo_logo_m.jpg", @"CFZ.jpg", @"CMA.png", @"FAZA.png", @"fl aq logo.jpg", @"NEI.png", @"PB.png", @"sante_fe_teaching_zoo.png", @"SWO Logo.jpg", @"TAMPA BAY AAZK.png", @"Zoo_Logo.jpg",  nil];
+    
+    sponsorImages = [[NSArray alloc] initWithObjects:
+                     @"brevard_zoo_logo_m.jpg",
+                     @"CFZ.jpg",
+                     @"Zoo_Logo.jpg",
+                     @"sante_fe_teaching_zoo.png",
+                     @"CMA.png",
+                     @"fl aq logo.jpg",
+                     @"SWO Logo.jpg",
+                     @"NEI.png",
+                     @"PB.png",
+                     @"ABI Logo.jpg",
+                     @"BGT Logo.png",
+                     @"FAZA.png",
+                     @"TAMPA BAY AAZK.png",  nil];
+    links = [[NSArray alloc] initWithObjects:
+             @"http://www.brevardzoo.org/",
+             @"http://www.centralfloridazoo.org/",
+             @"http://www.lowryparkzoo.org/",
+             @"http://www.sfcollege.edu/zoo/",
+             @"http://www.seewinter.com/",
+             @"http://www.flaquarium.org/",
+             @"https://seaworldparks.com/seaworld-orlando?&gclid=CNnZ_rOg5ssCFUQbgQodW_gLyg&dclid=CMvQhLSg5ssCFUQFgQod384IRQ",
+             @"http://naturalencounters.com/",
+             @"http://www.precisionbehavior.com/",
+             @"http://www.animaledu.com/Home/d/1",
+             @"https://seaworldparks.com/en/buschgardens-tampa/?&gclid=CM7sh5ig5ssCFYclgQodFi4MFg&dclid=CLD5i5ig5ssCFQsNgQodm1IJ_g",
+             @"http://www.flaza.org/zoos--aquariums.html",
+             @"http://tampabayaazk.weebly.com/",
+               nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -65,6 +95,11 @@
     imageView.image = [UIImage imageNamed:[sponsorImages objectAtIndex:indexPath.row]];
     
     return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *selectedLink = [links objectAtIndex:indexPath.item];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString: selectedLink]];
 }
 
 /*
