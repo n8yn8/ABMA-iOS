611f2a03ffb270b1387d49ff8eac0e0a94730f4b
Nathan Condell <natecondell@gmail.com>
2016-04-02 17:23:18 -0700

    Add president's welcome

--- ABMA/SideBarTableViewController.m
+++ ABMA/SideBarTableViewController.m
@@ -28,7 +28,7 @@
 {
     [super viewDidLoad];
     
-    _menuItems = @[@"logo", @"schedule", @"notes", @"info", @"sponsor", @"contact"];
+    _menuItems = @[@"logo", @"welcome", @"schedule", @"notes", @"info", @"sponsor", @"contact"];
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
