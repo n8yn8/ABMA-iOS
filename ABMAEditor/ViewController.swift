651daea730c75743d44c05434e5a9e4664289de5
Nathan Condell <natecondell@gmail.com>
2017-01-02 16:10:44 -0800

    Add Editor target.

---
+++ ABMAEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ABMAEditor
+//
+//  Created by Nathan Condell on 1/2/17.
+//  Copyright © 2017 Nathan Condell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
