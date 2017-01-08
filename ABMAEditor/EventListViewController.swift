57418effe8fd8c071be750e3d6006219a80e0728
Nathan Condell <natecondell@gmail.com>
2017-01-07 18:31:35 -0800

    Add stubbed tableview for list of events

---
+++ ABMAEditor/EventListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EventListViewController.swift
+//  ABMA
+//
+//  Created by Nathan Condell on 1/6/17.
+//  Copyright © 2017 Nathan Condell. All rights reserved.
+//
+
+import Cocoa
+
+class EventListViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
+    
+    let eventList = [Event]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return eventList.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        return NSView() //TODO
+    }
+
+}
---
+++ ABMAEditor/model/Event.swift
@@ -0,0 +1,13 @@
+//
+//  Event.swift
+//  ABMA
+//
+//  Created by Nathan Condell on 1/3/17.
+//  Copyright © 2017 Nathan Condell. All rights reserved.
+//
+
+import Foundation
+
+class Event: NSObject {
+    
+}
